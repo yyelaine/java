7ad0893a1594c6ba18160ae190d57a5cc4d35db9
yyelaine <yyelaine@foxmail.com>
2017-02-21 22:04:45 +0800

    测试静态变量

---
+++ Pvf.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Pvf {
+
+	/**
+	 * @param args
+	 */
+	static boolean Paddy;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Paddy);
+	}
+
+}
