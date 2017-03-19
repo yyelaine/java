d50ea61a5b35805a2f61adc96e68240a1b98c149
yyelaine <yyelaine@foxmail.com>
2017-03-19 23:36:12 +0800

    放苹果

--- Main5.java
+++ Main5.java
@@ -11,17 +11,24 @@ public class Main5 {
 	* @param m 苹果数目
 	* @param n 盘子数目数
 	* @return 放置方法总数
-	*
+	*放苹果分为两种情况，一种是有盘子为空，一种是每个盘子上都有苹果。 令(m,n)表示将m个苹果放入n个盘子中的摆放方法总数。 
+	*1.假设有一个盘子为空，则(m,n)问题转化为将m个苹果放在n-1个盘子上，即求得(m,n-1)即可 
+	*2.假设所有盘子都装有苹果，则每个盘子上至少有一个苹果，即最多剩下m-n个苹果，问题转化为将m-n个苹果放到n个盘子上 
+	*即求(m-n，n) 综上所述：  (m，n)=(m,n-1)+(m-n,n);
 	*/
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int m = scanner.nextInt();
+        while(scanner.hasNext())
+            {
+            int m = scanner.nextInt();
 		int n = scanner.nextInt();
 		int times = Main5.count(m, n);
-		System.out.print(times);
+		System.out.println(times);
+        }
+		
 	}
 	public static int count(int m, int n)
 	{
