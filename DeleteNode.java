5cb2fdfeb8d0e7f491a46dcc3d71ff46e2fd3211
yyelaine <yyelaine@foxmail.com>
2017-04-16 23:04:41 +0800

    删除单链表节点

---
+++ DeleteNode.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param node: the node in the list should be deleted
+     * @return: nothing
+     */
+    public void deleteNode(ListNode node) {
+        // write your code here
+        node.val = node.next.val;//将下个节点的值提前
+        node.next = node.next.next;//将下个节点提前
+        node = null;
+        
+    }
+}
